27d16f6e6019da183eef392c63974e77783d2686
Maryam Shahsavari <m.shahsavari65@gmail.com>
2021-06-13 00:42:57 +0430

    first commit of project structure

---
+++ src/main/java/com/game/shelem/ShelemApplication.java
@@ -0,0 +1,13 @@
+package com.game.shelem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/game/shelem/ShelemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.shelem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
